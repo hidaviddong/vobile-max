6579af2720cd0f6179fb91243fd956d8c5a31826
David Dong <hidaviddong@gmail.com>
2025-09-17 17:42:40 +0800

    feat: add baseURL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,7 @@
+import "../envConfig";
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
 
 export const authClient = createAuthClient({
   //you can pass client configuration here
+  baseURL: process.env.BETTER_AUTH_URL,
 });
